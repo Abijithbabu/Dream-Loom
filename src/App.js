e07497790cb349485a42c2ff91eb53fb5a55a8d6
Abhijith Babu <abijithb2002@gmail.com>
2024-02-11 23:55:15 +0530

    VITE replaced by create-react-app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import FaceDetection from './components/FaceDetection';
+
+function App() {
+  return (
+    <FaceDetection/>
+  );
+}
+
+export default App;
