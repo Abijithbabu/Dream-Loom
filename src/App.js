64e3add5dc6e7cf297a9ffedeac86dfd469ab95b
Abhijith Babu <abijithb2002@gmail.com>
2024-03-01 23:53:24 +0530

    theme & routes updated

--- src/App.js
+++ src/App.js
@@ -3,15 +3,29 @@ import { HashRouter } from "react-router-dom";
 import Routes from './routes/Routes';
 import './App.css';
 
-const darkTheme = createTheme({
+const theme = createTheme({
   palette: {
     mode: 'dark',
+  },
+   typography: {
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
   },
 });
 
 export default function App() {
   return (
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={theme}>
       <HashRouter>
         <Routes />
       </HashRouter>
