da985562708802ea59e5d9b2fffd8913c2d8ed57
Abhijith Babu <abijithb2002@gmail.com>
2024-05-01 23:43:49 +0530

    update:bg-img changed for all screens

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,12 +2,18 @@ import React from 'react'
 import WebLayout from './WebLayout'
 import MobileLayout from './MobileLayout'
 import { Container, Hidden } from '@mui/material';
-import { Outlet } from 'react-router-dom';
-import Img from '../assets/bg.jpeg'
+import { Outlet, useLocation } from 'react-router-dom';
+import Img1 from '../assets/bg5.jpg'
+import Img2 from '../assets/bg3.jpg'
+import Img3 from '../assets/bg4.jpg'
+import Img4 from '../assets/bg1.jpg'
+import Img5 from '../assets/bg.jpeg'
 const Layout = () => { 
+   const path = useLocation().pathname
+   const image = path === '/' ? Img1 : path === '/home' ? Img1 : path === '/favorites' ? Img2 : path === '/create' ? Img3 : path === '/library' ? Img4 : Img5
    return (
       <Container sx={{
-               backgroundImage: `url(${Img})`,
+               backgroundImage: `url(${image})`,
                backgroundSize: 'cover',
                backgroundRepeat: 'no-repeat',
                backgroundPosition: 'center',
